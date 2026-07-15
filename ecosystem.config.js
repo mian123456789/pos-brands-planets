daa81146376dbcf10204c5f3aa5679d6c0dd8a46
mian123456789 <mian123456789@users.noreply.github.com>
2026-07-15 23:42:39 +0500

    Revert "Add automatic customer SMS after print"
    This reverts commit 5863f247b3acecafcb68638d7793ac56c593e5f7.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,7 @@ module.exports = {
       script: "server.js",
       env: {
         PORT: 3000,
-        NODE_ENV: "production",
-        SMS_API_URL: process.env.SMS_API_URL || "",
-        SMS_API_TOKEN: process.env.SMS_API_TOKEN || "",
-        SMS_API_KEY: process.env.SMS_API_KEY || "",
-        SMS_SENDER: process.env.SMS_SENDER || "Brands Planets"
+        NODE_ENV: "production"
       }
     }
   ]
